d6dae7116bb08d17717cab34bbd3ea86e63ec749
Jerome Troy <jrtroy@udel.edu>
2023-04-20 11:31:45 -0400

    Added solver parameters to ring test, now working

--- test/experiments/expanding_ring.py
+++ test/experiments/expanding_ring.py
@@ -9,16 +9,27 @@ from uqdamage.fem.experiments import ExpandingRing
 
 
 r = 0.8
-h = 0.07
+h = 0.2
 ν = 0.1
-c = 1e-10
-Δt = 0.01
+c = 0.5
+Δt = 0.05
 tmax = 5
 
 nt = int(tmax / Δt)
-nsave = 3
+nsave = 1
 
-ring = ExpandingRing.RingProblem((r, h), ν, c, Δt)
+solver_params = {
+    "newton_solver" : {
+        "linear_solver" : "gmres",
+        "krylov_solver" : {
+            "monitor_convergence" : True,
+            "relative_tolerance" : 1e-8,
+        },
+        "maximum_iterations" : 100
+    }
+}
+ring = ExpandingRing.RingProblem((r, h), ν, c, Δt, solver_params=solver_params, 
+                                 α_f=0.2, α_m=0.2, η_m=1e-3, η_k=1e-3)
 
 plot(ring.mesh)
 plt.show()
@@ -29,7 +40,7 @@ plt.show()
 cs_vals = Σ.vector()[:]
 x = ring.Vθ.tabulate_dof_coordinates()
 cs_vals += 1
-cs_vals[np.abs(np.arctan2(x[:, 1], x[:, 0])) < 0.01] = 0.9
+cs_vals[np.abs(np.arctan2(x[:, 1], x[:, 0])) < np.pi/3] = 0.5
 Σ.vector()[:] = cs_vals
 
 ring.set_softening_fields(Σ, δσ)
