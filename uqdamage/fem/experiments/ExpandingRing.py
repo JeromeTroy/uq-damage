cbf2ef381988f1b4d15769db7e8f656f06e1c20f
Jerome Troy <jrtroy@udel.edu>
2023-04-20 11:32:15 -0400

    Removed boundary switching from ring

--- uqdamage/fem/experiments/ExpandingRing.py
+++ uqdamage/fem/experiments/ExpandingRing.py
@@ -10,7 +10,7 @@ from collections.abc import Iterable
 
 from fenics import (
     Expression, DirichletBC, solve, 
-    set_log_level, Mesh
+    set_log_level, Mesh, sqrt
 )
 
 from uqdamage.fem.DamageBase import DamageProblem
@@ -32,7 +32,7 @@ class RingProblem(DamageProblem):
         return (x[0]**2 + x[1]**2 < self.rm**2) and on_boundary
 
     def __init__(self, mesh_params, ν, c, Δt,
-                 ρ=1, E=1,
+                 ρ=1, E=1, α_f=0.4,
                  **kwargs):
         """
         Constructor for expanding ring problem
@@ -91,23 +91,24 @@ class RingProblem(DamageProblem):
         self.c = c
 
         # construct domain and set boundaries
-        self.rm = 0.5 * (r0+r1)  
+        self.r_mid = 0.5 * (r0 + r1)
+        self.r = r0
+
         # Sub domain for inner ring
         def inner_boundary(x, on_boundary):
-            return (x[0]**2 + x[1]**2 < self.rm**2) and on_boundary
+            return sqrt(x[0]**2 + x[1]**2) < self.r_mid and on_boundary
 
         # construct expansion bcs
-        self.u_inner = Expression(("c*t*x[0]/sqrt(x[0]*x[0] + x[1]*x[1])",
-                            "c*t*x[1]/sqrt(x[0]*x[0] + x[1]*x[1])"),
-                    element=self.V.ufl_element(), c = self.c, t = 0)
+        self.u_inner = Expression(("c*t*x[0]/r",
+                            "c*t*x[1]/r"),
+                    element=self.V.ufl_element(), c = self.c, t = 0, r=self.r)
         self.bcs.append(DirichletBC(self.V, self.u_inner, inner_boundary))
 
-        return None
+        self.α_f_float = α_f 
 
-
-    def set_inner_bc(self, BC):
-        self.bcs = []
-        self.bcs.append(BC)
+    # def set_inner_bc(self, BC):
+    #     self.bcs = []
+    #     self.bcs.append(BC)
 
 
     # def set_constant_accel(self, a):
@@ -122,44 +123,68 @@ class RingProblem(DamageProblem):
 
     #     self.set_inner_bc(DirichletBC(self.V, self.u_inner, inner_boundary))
 
-    def set_free_expansion(self):
-        self.bcs = []
+    # def set_free_expansion(self):
+    #     self.bcs = []
 
-    def integrate(self, nsteps, nsave, verbose=True, 
-        stress_list=None, off_time=None, **save_kwargs):
+    def update_boundary(self, t):
+        self.u_inner.t = t - self.α_f_float * self.Δt
+
+    def integrate(self, nsteps, nsave, record_times=False, **save_kwargs):
         """
-        Integrate the expanding ring problem over nsteps.  Save to disk in
-        the output_file (XDMF format) evrery nsave steps.
+        Solve the problem forward in time
+
+        Input:
+            nsteps : int > 0
+                number of time steps to take.
+                The maximum integration time will be
+                self.Δt * nsteps.
+            nsave : int > 0
+                the number of steps in between savings.
+                So if nsave = 2, the problem will save every-other
+                time step.
+        Optional Key word arguments (for saving)
+            xdmf_file : main output location (viewed with paraview)
+            damage_file : hdf5 file for damage variable  (scalar output)
+            stress_file : hdf5 file for stress variable (tensor output)
+            strain_file : hdf5 file for strain variable (tensor output)
+            velocity_file : hdf5 file for velocity variable (vector output)
+
+            These are fed directly to DamageProblem.save_state()
+
         """
 
-        swap_bc_flag = False
-        if off_time is not None:
-            swap_bc_flag = True
+        # swap_bc_flag = False
+        # if off_time is not None:
+        #     swap_bc_flag = True
         t_vals = float(self.Δt) * np.arange(nsteps+1)
 
         self.compute_stresses()
         self.compute_damage()
+
         self.save_state(t_vals[0], **save_kwargs)
 
         set_log_level(30)
 
-        for i in range(1,t_vals.size):
+        for i in range(1, t_vals.size):
             t = t_vals[i]
 
-            if swap_bc_flag:
-                if t > off_time:
-                    # set to free boundaray condition
-                    self.set_boundary_conditions([])
-                    swap_bc_flag = False
+            # if swap_bc_flag:
+            #     if t > off_time:
+            #         # set to free boundaray condition
+            #         self.set_boundary_conditions([])
+            #         swap_bc_flag = False
 
             # update applied displacement at the G-α intermediate time
-            self.u_inner.t = t - float(self.α_f) * float(self.Δt)
+            self.update_boundary(t)
+            logging.debug(f"Main solve at t = {t}")
 
-            solve(self.Fu==0, self.u, bcs=self.bcs, J = self.Ju,
-                form_compiler_parameters={"optimize": True})
+            solve(self.Fu == 0, self.u, bcs=self.bcs, J = self.Ju,
+                form_compiler_parameters={"optimize": True},
+                solver_parameters=self.solver_params)
+            
             update_fields(self.u, self.u_old, self.v_old, self.a_old,
                 self.Δt, self.β, self.γ)
-            #self.compute_stresses(stress_list=stress_list)
+            self.compute_stresses()
             self.compute_damage()
 
             if i%nsave==0:
