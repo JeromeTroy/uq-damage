b2c7e1a45f680120504ff7291716b15af99259de
Jerome Troy <jrtroy@udel.edu>
2023-04-21 16:19:02 -0400

    Added random ring test

---
+++ test/postprocess/expanding_ring.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import logging
+logging.getLogger().setLevel(logging.INFO)
+
+from fenics import *
+
+from uqdamage.fem.experiments import ExpandingRing
+from uqdamage.fem.RandomField import RandomLogNormalField
+
+
+r = 0.8
+h = 0.05
+ν = 0.1
+c = 0.5
+Δt = 0.05
+tmax = 5
+
+δ = 0.1
+ℓ = 0.1
+δσ = Constant(0.1)
+num_modes = 50
+
+nt = int(tmax / Δt)
+nsave = 1
+
+ring = ExpandingRing.RingProblem((r, h), ν, c, Δt, 
+                                 α_f=0.2, α_m=0.2, η_m=1e-3, η_k=1e-3)
+
+plot(ring.mesh)
+plt.show()
+
+k = lambda x, y: δ * np.exp(-np.linalg.norm(x - y)**2 / (2 * ℓ**2))
+mean = Constant(1)
+max_range = 3 * ℓ
+X = RandomLogNormalField(mean=mean, 
+                         kernel=k, 
+                         space=ring.Vθ, 
+                         num_modes=num_modes,
+                         max_range=max_range)
+
+Σ = X.sample_field()
+art = plot(Σ)
+plt.colorbar(art)
+plt.show()
+
+ring.set_softening_fields(Σ, δσ)
+
+xdmf = XDMFFile("ring.xdmf")
+xdmf.parameters["flush_output"] = True
+xdmf.parameters["functions_share_mesh"] = True
+xdmf.parameters["rewrite_function_mesh"] = False
+xdmf.write(Σ)
+ring.integrate(nt, nsave, xdmf_file=xdmf)
+print("Done")
