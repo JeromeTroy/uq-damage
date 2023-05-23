28b45f5a7cf89567010951b454ad58acdcb3243b
Jerome Troy <jrtroy@udel.edu>
2023-05-23 18:40:00 -0400

    Added random yield stress to expanding ring test/demo

--- test/experiments/expanding_ring.py
+++ test/experiments/expanding_ring.py
@@ -6,17 +6,24 @@ logging.getLogger().setLevel(logging.INFO)
 from fenics import *
 
 from uqdamage.fem.experiments import ExpandingRing
+from uqdamage.fem.RandomField import RandomLogNormalField
 
+from random_field_decomposition import laplacian_decomposition
 
 r = 0.8
-h = 0.2
+h = 0.05
 ν = 0.1
 c = 0.5
 Δt = 0.05
-tmax = 5
+tmax = 1
 
 nt = int(tmax / Δt)
 nsave = 1
+
+num_modes = 50
+correlation_length = 0.5
+eig_val_scaling = 0.1
+eig_gen = lambda modes: laplacian_decomposition(modes, correlation_length, scaling=eig_val_scaling)
 
 solver_params = {
     "newton_solver" : {
@@ -31,17 +38,13 @@ solver_params = {
 ring = ExpandingRing.RingProblem((r, h), ν, c, Δt, solver_params=solver_params, 
                                  α_f=0.2, α_m=0.2, η_m=1e-3, η_k=1e-3)
 
-plot(ring.mesh)
-plt.show()
-
 δσ = Constant(0.1)
-Σ = Function(ring.Vθ)
+X = RandomLogNormalField(Constant(1), eig_gen=eig_gen, 
+                         space=ring.Vθ, num_modes=num_modes)
 
-cs_vals = Σ.vector()[:]
-x = ring.Vθ.tabulate_dof_coordinates()
-cs_vals += 1
-cs_vals[np.abs(np.arctan2(x[:, 1], x[:, 0])) < np.pi/3] = 0.5
-Σ.vector()[:] = cs_vals
+np.random.seed(1)
+Σ = X.sample_field()
+
 
 ring.set_softening_fields(Σ, δσ)
 
